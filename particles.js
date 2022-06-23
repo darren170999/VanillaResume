348fcaf1412108650c826317bd3a31ac810db089
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2022-06-24 02:23:05 +0800

    h

--- particles.js
+++ particles.js
@@ -9,118 +9,123 @@ var pJS = function(tag_id, params){
         w: canvas_el.offsetWidth,
         h: canvas_el.offsetHeight
       },
-        "particles": {
-          "number": {
-            "value": 294,
-            "density": {
-              "enable": false,
-              "value_area": 1815.039575754227
-            }
-          },
-          "color": {
-            "value": "#ffffff"
-          },
-          "shape": {
-            "type": "circle",
-            "stroke": {
-              "width": 0,
-              "color": "#000000"
-            },
-            "polygon": {
-              "nb_sides": 6
-            },
-            "image": {
-              "src": "http://wiki.lexisnexis.com/academic/images/f/fb/Itunes_podcast_icon_300.jpg",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 0.3447335930860874,
-            "random": false,
-            "anim": {
-              "enable": false,
-              "speed": 1,
-              "opacity_min": 0.1,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 5,
-            "random": true,
-            "anim": {
-              "enable": false,
-              "speed": 17.052652093416064,
-              "size_min": 0.1,
-              "sync": true
-            }
-          },
-          "line_linked": {
-            "enable": false,
-            "distance": 150,
-            "color": "#ffffff",
-            "opacity": 0.4,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 14,
-            "direction": "left",
-            "random": true,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-            }
+      particles: {
+        number: {
+          value: 400,
+          density: {
+            enable: true,
+            value_area: 800
           }
         },
-        "interactivity": {
-          "detect_on": "canvas",
-          "events": {
-            "onhover": {
-              "enable": true,
-              "mode": "grab"
-            },
-            "onclick": {
-              "enable": true,
-              "mode": "repulse"
-            },
-            "resize": true
+        color: {
+          value: '#fff'
+        },
+        shape: {
+          type: 'circle',
+          stroke: {
+            width: 0,
+            color: '#ff0000'
           },
-          "modes": {
-            "grab": {
-              "distance": 200,
-              "line_linked": {
-                "opacity": 1
-              }
-            },
-            "bubble": {
-              "distance": 400,
-              "size": 40,
-              "duration": 2,
-              "opacity": 8,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 200,
-              "duration": 0.4
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
-            }
+          polygon: {
+            nb_sides: 5
+          },
+          image: {
+            src: '',
+            width: 100,
+            height: 100
           }
         },
-        "retina_detect": true
+        opacity: {
+          value: 1,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 2,
+            opacity_min: 0,
+            sync: false
+          }
+        },
+        size: {
+          value: 20,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 20,
+            size_min: 0,
+            sync: false
+          }
+        },
+        line_linked: {
+          enable: true,
+          distance: 100,
+          color: '#fff',
+          opacity: 1,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 2,
+          direction: 'none',
+          random: false,
+          straight: false,
+          out_mode: 'out',
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 3000,
+            rotateY: 3000
+          }
+        },
+        array: []
+      },
+      interactivity: {
+        detect_on: 'canvas',
+        events: {
+          onhover: {
+            enable: true,
+            mode: 'grab'
+          },
+          onclick: {
+            enable: true,
+            mode: 'push'
+          },
+          resize: true
+        },
+        modes: {
+          grab:{
+            distance: 100,
+            line_linked:{
+              opacity: 1
+            }
+          },
+          bubble:{
+            distance: 200,
+            size: 80,
+            duration: 0.4
+          },
+          repulse:{
+            distance: 200,
+            duration: 0.4
+          },
+          push:{
+            particles_nb: 4
+          },
+          remove:{
+            particles_nb: 2
+          }
+        },
+        mouse:{}
+      },
+      retina_detect: false,
+      fn: {
+        interact: {},
+        modes: {},
+        vendors:{}
+      },
+      tmp: {}
     };
   
     var pJS = this.pJS;
-  
     /* params settings */
     if(params){
       Object.deepExtend(pJS, params);
